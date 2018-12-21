7ecd195aa9c2de70515bb161a21d4615015eb775
sookidak <sookidak@gmail.com>
2018-12-21 16:44:09 +0900

    모바일 1차 완료

--- js/imc_common.js
+++ js/imc_common.js
@@ -1,29 +1,67 @@
 (function ( $ ) {
-	// left 영역 
-	$.fn.showLeft = function(options) {
-		var settings = $.extend({
-		}, options );
-		
-		let $aside = $(this);
+	function asideOpen(){
+		// left 영역 
+		let $aside = $('.js-aside');
 		let $openBtn = $('.js-menu-open');
 		let asideW = $(window).outerWidth();
 
 		$aside.hide().css('width',asideW);
-		return this.each(function() {
-			
-			$openBtn.click(function(){
-				if($aside.is(':hidden')){
-					$aside.stop().show().animate({'right': '0'},function(){
-						$('body').css('overflow','hidden');	
-					});
-					
-				}else{
-					$aside.stop().animate({'right': '100%'}).hide('100');
-					$('body').css('overflow','auto');
-				}
-			})
-		});
+		$openBtn.click(function(){
+			if($aside.is(':hidden')){
+				$aside.stop().show().animate({'right': '0'});
+				$('body').addClass('scrollOff').on('scroll touchmove', function(e){
+					e.preventDefault();
+				});
+
+			}else{
+				$aside.stop().animate({'right': '100%'}).hide('100');
+				$('body').removeClass('scrollOff').off('scroll touchmove');
+			}
+		})
 	};
+	asideOpen();
+
+	// quick 메뉴
+	function quickOpen(){
+		let $quickmenBbtn = $('.js-quickbtn');
+		let $quickmenuDiv = $('.js-quickwrap');
+		let $quickCloseBtn = $('.js-quickwrap').find('.js-quickclose');
+	
+		$quickmenuDiv.hide();
+		$quickmenBbtn.click(function(){
+			if($quickmenuDiv.is(':hidden')){
+				$quickmenuDiv.show();
+			}
+		});
+		$quickCloseBtn.click(function(){
+			if($quickmenuDiv.is(':visible')){
+				$quickmenuDiv.hide();
+			}
+		})
+	}
+	quickOpen();
+	
+
+	// 상단으로 가기
+	function scrollTop(){
+		let $scrolltopBtn = $('.js-scrolltop');
+
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$scrolltopBtn.fadeIn();
+			} else {
+				$scrolltopBtn.fadeOut();
+			}
+		});
+		$scrolltopBtn.click(function() {
+			$('html, body').animate({
+				scrollTop : 0
+			}, 400);
+			return false;
+		});
+	}
+	scrollTop();
+
 
 	// lnb 메뉴
 	$.fn.showLnb = function(options){
@@ -32,16 +70,34 @@
 		}, options );
 
 		let $nav = $('.js-nav');
-		let $navlist = $nav.children('li');
+		let $navlist = $nav.find('li');
 		let $menuFirst = $navlist.find('.btn-lnb');
+		let $menuSecond = $navlist.find('.lnb-2depth>li>a');
 
-		return this.each(function() {
+		$menuFirst.click(function(){
+			var $selLi = $(this).parent('li');
 			
-			$menuFirst.click(function(){
-				$(this).parent('li').addClass(opt.activeClass);
-			});
+			if($selLi.hasClass(opt.activeClass)){
+				$selLi.removeClass(opt.activeClass);
+			}else{
+				$selLi.addClass(opt.activeClass);
+			}
+			return false;
 		});
+		$menuSecond.click(function(){
+			var $selLi = $(this).parent('li');
+
+			if($selLi.hasClass(opt.activeClass)){
+				$selLi.removeClass(opt.activeClass);
+			}else{
+				$selLi.addClass(opt.activeClass);
+			}
+			return false;
+
+		})
+
 	};
+
 
 }( jQuery ));
 
