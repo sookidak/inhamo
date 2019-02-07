01fb85b4f48e460b3b22abf9f265c27a5f714854
ljhmw6 <chorika@naver.com>
2019-02-07 16:05:20 +0900

    수정사항 적용(가로 테이블 및 의료진 디폴트 사진)

--- js/imc_common.js
+++ js/imc_common.js
@@ -124,6 +124,33 @@
 		})
 	};
 
-	// 이미지 확대/축소 
+	//테이블 가로 스크롤 적용
+	function scrollTable() {
+		$('.tableWrapper').each(function () {
+			if ($(this).get(0).scrollWidth > ($(this).get(0).clientWidth) + 1) {
+				$(this).addClass('scrollable');
+				if (($(this).outerWidth() + $(this).scrollLeft()) < $(this).get(0).scrollWidth) {
+					$(this).addClass('scrollRight');
+				} else {
+					$(this).removeClass('scrollRight');
+				}
+
+				if ($(this).scrollLeft() != 0) {
+					$(this).addClass('scrollLeft');
+				} else {
+					$(this).removeClass('scrollLeft');
+				}
+			} else {
+				$(this).removeClass('scrollable')
+				.removeClass('scrollLeft')
+				.removeClass('scrollRight');
+			}
+		});
+	}
+	scrollTable();
+	$('.tableWrapper').scroll(function () { scrollTable(); });
+	$(window).bind('orientationchange resize', function (event) { scrollTable(); });
+
 
 }( jQuery ));
+
